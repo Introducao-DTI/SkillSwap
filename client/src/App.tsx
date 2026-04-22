d2fe74b7a4b6e8261ae3932a969ce0eb96f40a58
Rafael (Falves) Alves <rafalves106@icloud.com>
2026-04-22 11:54:01 -0300

    feat: Consome api de tokens de convite no frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,35 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate, useSearchParams } from "react-router-dom";
 import { AuthRoutes } from "./features/auth/routes/AuthRoutes";
+import { useEffect } from "react";
+import { useAppDispatch } from "./store/hooks";
+import {
+  setEmailConvite,
+  setNomeConvite,
+  setRoleUsuario,
+} from "./store/slices/authSlice";
+import { conviteApi } from "./features/auth/api/conviteApi";
 
 const App = () => {
+  const [searchParams] = useSearchParams();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+
+    if (!token) return;
+
+    conviteApi
+      .validarToken(token)
+      .then((convite) => {
+        dispatch(setEmailConvite(convite.email));
+        dispatch(setNomeConvite(convite.nome));
+        dispatch(setRoleUsuario(convite.role));
+        navigate("/bem-vindo");
+      })
+      .catch(() => navigate("/token-invalido"));
+  }, []);
+
   return (
     <Routes>
       <Route path="/*" element={<AuthRoutes />} />
