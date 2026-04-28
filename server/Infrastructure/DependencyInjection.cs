fe4da9b9423080611e8611c59589fdb7a216c0f0
Rafael Martins Alves <rafael.alves@dtidigital.com.br>
2026-04-28 11:06:48 -0300

    chore(infra): registrar MasterDbContext e IEmpresaRepository no DI

--- server/Infrastructure/DependencyInjection.cs
+++ server/Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SkillSwap.Core.Usuarios.Ports.Out;
+using SkillSwap.Core.Empresas.Ports.Out;
 using SkillSwap.Infrastructure.Data;
 using SkillSwap.Infrastructure.Repositories;
 using SkillSwap.Infrastructure.Security;
@@ -11,15 +12,21 @@ namespace SkillSwap.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+        services.AddScoped<IEmpresaRepository, EmpresaRepository>();
         services.AddScoped<IVerificacaoRepository, VerificacaoRepository>();
         services.AddScoped<IConviteRepository, ConviteRepository>();
 
         services.AddScoped<ISenhaService, SenhaService>();
         services.AddScoped<IEmailService, SmtpEmailService>();
 
+
+        services.AddDbContext<MasterDbContext>(options =>
+            options.UseSqlite(
+                configuration.GetConnectionString("Master")
+            ));
 
         services.AddDbContext<SkillSwapDbContext>(options =>
             options.UseSqlite("Data Source=skillswap.db"));
